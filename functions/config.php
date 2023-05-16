d5e2ab490307502d758b9b98e5eea2c91a745028
Joey Sumalpong <129673616+malpongsu@users.noreply.github.com>
2023-05-17 02:35:10 +0800

    Update config.php

--- functions/config.php
+++ functions/config.php
@@ -1,5 +1,17 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','raspberry_rfid_db');
+// Database configuration
+$db_host = 'localhost';
+$db_user = 'rfid_admin';
+$db_pass = 'rfid_system';
+$db_name = 'raspberry_pi_rfid_system';
+
+// Create database connection
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+// Check database connection
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
 
 ?>
